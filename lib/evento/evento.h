0e5bd2eaadb827db02e3aa4933d980fa23e78e2e
Krud3 <juan.sebastian.molina@correounivalle.edu.co>
2023-12-12 12:36:37 -0500

    se modifican las clases para su funcionamiento y compilacion correcta teniendo en cuenta el enunciado

--- lib/evento/evento.h
+++ lib/evento/evento.h
@@ -1,1 +1,23 @@
-#dummy
+#ifndef EVENTO_H
+#define EVENTO_H
+
+#include "../animales/animal.h"
+#include "../escena/escena.h"
+
+class Evento{
+
+    public:
+        Evento(int n, int m, int k);
+        ~Evento();
+        int getN();
+        int getM();
+        int getK();
+        Animal animalMasConcurrente();
+        Animal animalMenosConcurrente();
+        Escena escenaMenorGrandeza();
+        Escena escenaMayorGrandeza();
+        double getPromedioGrandeza();
+
+};
+
+#endif
