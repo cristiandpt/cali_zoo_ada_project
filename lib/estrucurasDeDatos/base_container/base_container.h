4d2470dc5baf71b5a94ef2b8adaded0b0c20fa07
Krud3 <juan.sebastian.molina@correounivalle.edu.co>
2023-12-14 00:36:44 -0500

    se acomoda base container

--- lib/estrucurasDeDatos/base_container/base_container.h
+++ lib/estrucurasDeDatos/base_container/base_container.h
@@ -2,19 +2,14 @@
 template <typename T>
 class BaseContainer{
    public:
-      // pure virtual function providing interface framework.
-      virtual std::size_t  max() = 0;
-      virtual std::size_t  min() = 0;
-      virtual std::size_t  aggregate() = 0;
-      virtual T& operator[](std::size_t  index) = 0;
-      virtual std::size_t  sizeC() = 0;
-      virtual std::size_t  mean() = 0;
-      virtual ~BaseContainer() {}
-
-    virtual std::size_t max() = 0;
-    virtual std::size_t min() = 0;
-    virtual T& operator[](std::size_t index) = 0;
-    virtual std::size_t sizeC() = 0;
+    // pure virtual function providing interface framework.
+    virtual std::size_t  max() = 0;
+    virtual std::size_t  min() = 0;
+    virtual std::size_t  aggregate() = 0;
+    virtual T& operator[](std::size_t  index) = 0;
+    virtual std::size_t  sizeC() = 0;
+    virtual std::size_t  mean() = 0;
+    virtual ~BaseContainer() {}    
     virtual void insert(T& value) = 0;
     /*virtual std::pair<BaseContainer<T>*,BaseContainer<T>*> divide() = 0;
     virtual BaseContainer<T>* merge(BaseContainer<T>* other) = 0;*/
